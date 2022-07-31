466295637131ac1851ffbe215c9b6b740b0f6cd8
Koen Schipper <koen.schipper@me.com>
2022-07-31 18:22:30 +0200

    some minor changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,15 +10,10 @@ require("hardhat-deploy")
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const MAINNET_RPC_URL =
-    process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ""
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const KOVAN_RPC_URL =
-    process.env.KOVAN_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY =
-    process.env.PRIVATE_KEY ||
-    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
@@ -33,17 +28,17 @@ module.exports = {
         localhost: {
             chainId: 31337,
         },
-        kovan: {
-            url: KOVAN_RPC_URL,
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
             accounts: [PRIVATE_KEY],
-            chainId: 42,
+            chainId: 4,
             blockConfirmations: 6,
         },
     },
     solidity: {
         compilers: [
             {
-                version: "0.8.8",
+                version: "0.8.9",
             },
             {
                 version: "0.6.12",
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,4 +1,26 @@
-async function main() {}
+const { getNamedAccounts, ethers } = require("hardhat")
+const { getWeth } = require("../scripts/getWeth")
+
+async function main() {
+    await getWeth()
+    const { deployer } = await getNamedAccounts()
+
+    // 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        deployer
+    )
+    const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt(
+        "ILendingPool",
+        lendingPoolAddress,
+        deployer
+    )
+}
 
 main()
     .then(() => process.exit(0))
